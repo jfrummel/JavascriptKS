08c689d75f91ef806d6495f3771cbed67d476519
unknown <jfrummel@gmail.com>
2018-09-05 17:14:09 -0500

    Added some stuff

---
+++ app.js
@@ -0,0 +1,20 @@
+let firstName = 'Jeremy';
+
+const numStates = 50;
+
+let sum = 5 + 4;
+
+if (firstName.charAt(0) == 'L') {
+    console.log('Back of the line!');
+} else {
+    console.log('Next');
+};
+
+// This actually worked.
+
+function sayHello() {
+    console.log('Hello World!');
+};
+
+sayHello();
+
